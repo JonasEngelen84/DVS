2019e5ef7d289438a2f56ac411f37be2e81e3bd1
Jonas Engelen <jonasEngelen@hotmail.de>
2024-05-09 20:33:50 +0200

    AddClothesComponent eingefügt

--- ViewModels/DVSViewModel.cs
+++ ViewModels/DVSViewModel.cs
@@ -11,26 +11,26 @@ namespace DVS.ViewModels
         public EmployeesClothesListViewViewModel EmployeesClothesListViewViewModel { get; }
         public ClothesListViewViewModel ClothesListViewViewModel { get; }
 
-        //public ICommand FilterCommand { get; }
-        //public ICommand AddCommand { get; }
-        //public ICommand EditCommand { get; }
-        //public ICommand SafeCommand { get; }
-        //public ICommand PrintCommand { get; }
-        //public ICommand PlusCommand { get; }
-        //public ICommand MinusCommand { get; }
+        public ICommand FilterCommand { get; }
+        public ICommand AddCommand { get; }
+        public ICommand EditCommand { get; }
+        public ICommand SafeCommand { get; }
+        public ICommand PrintCommand { get; }
+        public ICommand PlusCommand { get; }
+        public ICommand MinusCommand { get; }
 
         public DVSViewModel()
         {
             EmployeesClothesListViewViewModel = new();
             ClothesListViewViewModel = new();
-            
-            //FilterCommand = new FilterCommand(this);
-            //AddCommand = new AddCommand(this);
-            //EditCommand = new EditCommand(this);
-            //SafeCommand = new SafeCommand(this);
-            //PrintCommand = new PrintCommand(this);
-            //PlusCommand = new PlusCommand(this);
-            //MinusCommand = new MinusCommand(this);
+
+            FilterCommand = new FilterCommand(this);
+            AddCommand = new AddCommand(this);
+            EditCommand = new EditCommand(this);
+            SafeCommand = new SafeCommand(this);
+            PrintCommand = new PrintCommand(this);
+            PlusCommand = new PlusCommand(this);
+            MinusCommand = new MinusCommand(this);
         }
     }
 }
