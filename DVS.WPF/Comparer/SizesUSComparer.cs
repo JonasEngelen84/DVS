572c82a3054c34cb70106d5a85cdff2222bb276e
Jonas Engelen <jonasEngelen@hotmail.de>
2025-02-21 12:37:52 +0100

    Zwischenspeichern: Sortierung der Sizes-Listen der Clothes in HeadView

---
+++ DVS.WPF/Comparer/SizesUSComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace DVS.WPF.Comparer
+{
+    class SizesUSComparer : IComparer
+    {
+        private static readonly List<string> _sizeOrder = new()
+        {
+            "XS", "S", "M", "L", "XL", "XXL", "3XL", "4XL", "5XL", "6XL"
+        };
+
+        public int Compare(object? x, object? y)
+        {
+            //if (x == null || y == null) return 0;
+
+            //int indexX = _sizeOrder.IndexOf(x);
+            //int indexY = _sizeOrder.IndexOf(y);
+
+            //if (indexX == -1) indexX = int.MaxValue;
+            //if (indexY == -1) indexY = int.MaxValue;
+
+            //return indexX.CompareTo(indexY);
+            return 1;
+        }
+    }
+}
