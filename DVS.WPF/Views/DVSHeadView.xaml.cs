e5beaa2d4bd05683b9b1290f6dd1c8a80c45b2dc
Jonas Engelen <jonasEngelen@hotmail.de>
2025-03-18 11:59:00 +0100

    UI der Suchfunktionen implementiert, logik noch ausstehend

--- DVS.WPF/Views/DVSHeadView.xaml.cs
+++ DVS.WPF/Views/DVSHeadView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace DVS.WPF.Views
 {
@@ -7,6 +8,16 @@ namespace DVS.WPF.Views
         public DVSHeadView()
         {
             InitializeComponent();
+        }
+
+        private void ClearEmployeeSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchEmployeeTextBox.Text = string.Empty;
+        }
+
+        private void ClearClothesSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchClothesTextBox.Text = string.Empty;
         }
     }
 }
